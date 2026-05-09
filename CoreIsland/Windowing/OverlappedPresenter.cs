b6fb95cbd89d3d82f6f8ae34c25ba020dad005df
k1mlka <k1mlka@outlook.com>
2026-05-09 10:24:44 +0800

    Implement OverlappedPresenter.SetBorderAndTitleBar

--- CoreIsland/Windowing/OverlappedPresenter.cs
+++ CoreIsland/Windowing/OverlappedPresenter.cs
@@ -1,5 +1,4 @@
 using Windows.Win32;
-using Windows.Win32.Foundation;
 using Windows.Win32.UI.WindowsAndMessaging;
 
 namespace CoreIsland.Windowing;
@@ -11,6 +10,8 @@ public sealed class OverlappedPresenter : AppWindowPresenter
     private bool _isResizable = true;
     private bool _isMaximizable = true;
     private bool _isMinimizable = true;
+    private bool _hasBorder = true;
+    private bool _hasTitleBar = true;
 
     public bool IsResizable
     {
@@ -35,25 +36,52 @@ public sealed class OverlappedPresenter : AppWindowPresenter
 
     internal OverlappedPresenter(AppWindow appWindow) : base(appWindow) { }
 
+    public void SetBorderAndTitleBar(bool hasBorder, bool hasTitleBar)
+    {
+        _hasBorder = hasBorder;
+        _hasTitleBar = hasTitleBar;
+        ApplyStyles();
+    }
+
     private void ApplyStyles()
     {
         var hwnd = AppWindow.Hwnd;
-        var style = (WINDOW_STYLE)(nint)PInvoke.GetWindowLongAnyCPU(hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
+        var style = (WINDOW_STYLE)PInvoke.GetWindowLongAnyCPU(hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
 
-        if (_isResizable)
-            style |= WINDOW_STYLE.WS_THICKFRAME;
+        if (!_hasBorder)
+        {
+            style &= ~(WINDOW_STYLE.WS_CAPTION | WINDOW_STYLE.WS_SYSMENU |
+                        WINDOW_STYLE.WS_THICKFRAME | WINDOW_STYLE.WS_BORDER |
+                        WINDOW_STYLE.WS_MAXIMIZEBOX | WINDOW_STYLE.WS_MINIMIZEBOX);
+        }
         else
-            style &= ~WINDOW_STYLE.WS_THICKFRAME;
+        {
+            if (_hasTitleBar)
+            {
+                style |= WINDOW_STYLE.WS_CAPTION;
+                style |= WINDOW_STYLE.WS_SYSMENU;
 
-        if (_isMaximizable)
-            style |= WINDOW_STYLE.WS_MAXIMIZEBOX;
-        else
-            style &= ~WINDOW_STYLE.WS_MAXIMIZEBOX;
+                if (_isMaximizable)
+                    style |= WINDOW_STYLE.WS_MAXIMIZEBOX;
+                else
+                    style &= ~WINDOW_STYLE.WS_MAXIMIZEBOX;
 
-        if (_isMinimizable)
-            style |= WINDOW_STYLE.WS_MINIMIZEBOX;
-        else
-            style &= ~WINDOW_STYLE.WS_MINIMIZEBOX;
+                if (_isMinimizable)
+                    style |= WINDOW_STYLE.WS_MINIMIZEBOX;
+                else
+                    style &= ~WINDOW_STYLE.WS_MINIMIZEBOX;
+            }
+            else
+            {
+                style &= ~(WINDOW_STYLE.WS_CAPTION | WINDOW_STYLE.WS_SYSMENU |
+                            WINDOW_STYLE.WS_MAXIMIZEBOX | WINDOW_STYLE.WS_MINIMIZEBOX);
+            }
+
+            if (_isResizable)
+                style |= WINDOW_STYLE.WS_THICKFRAME;
+            else
+                style &= ~WINDOW_STYLE.WS_THICKFRAME;
+        }
 
         PInvoke.SetWindowLongAnyCPU(hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE, (nint)style);
         PInvoke.SetWindowPos(hwnd, default, 0, 0, 0, 0,
