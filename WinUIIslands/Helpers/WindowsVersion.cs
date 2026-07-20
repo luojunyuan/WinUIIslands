104f1ceb225fccceefbd72c1d00afa509c64f984
k1mlka <k1mlka@outlook.com>
2026-07-20 20:29:01 +0800

    refactor: use managed Windows version checks

--- WinUIIslands/Helpers/WindowsVersion.cs
+++ WinUIIslands/Helpers/WindowsVersion.cs
@@ -1,22 +1,8 @@
-﻿using Windows.Win32.System.SystemInformation;
-
-namespace WinUIIslands;
+﻿namespace WinUIIslands;
 
 internal static class WindowsVersion
 {
-    private static readonly uint s_build = QueryBuildNumber();
+    public static bool IsWin11OrNewer => OperatingSystem.IsWindowsVersionAtLeast(10, 0, 22000);
 
-    public static bool IsWin11OrNewer => s_build >= 22000;
-
-    public static bool Is22H2OrNewer => s_build >= 22621;
-
-    private static unsafe uint QueryBuildNumber()
-    {
-        OSVERSIONINFOW version = new()
-        {
-            dwOSVersionInfoSize = (uint)sizeof(OSVERSIONINFOW),
-        };
-
-        return Windows.Wdk.PInvoke.RtlGetVersion(ref version) >= 0 ? version.dwBuildNumber : 0;
-    }
+    public static bool Is22H2OrNewer => OperatingSystem.IsWindowsVersionAtLeast(10, 0, 22621);
 }
