177825a692140e4191858d982fb97f99899eb41d
k1mlka <k1mlka@outlook.com>
2026-07-23 02:13:25 +0800

    feat: add Exe IslandsApp1 sample

---
+++ IslandsApp1/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Windows.ApplicationModel.Activation;
+
+namespace IslandsApp1
+{
+    public sealed partial class App : WinUIIslands.Application
+    {
+        private MainWindow? _window;
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnIslandLaunched(LaunchActivatedEventArgs e)
+        {
+            _window = new MainWindow();
+            _window.Activate();
+        }
+    }
+}
---
+++ IslandsApp1/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml;
+
+namespace IslandsApp1
+{
+    public sealed partial class MainWindow : WinUIIslands.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(AppTitleBar);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) =>
+            TestButton.Content = "Clicked!";
+    }
+}
